05cc82bac589d9c95551cdc25d28f93d856dbd1f
flightofthefox <foxy@borderlinelabs.com>
2025-10-23 07:15:04 +0800

    Add key support to parser.

--- crates/sql/src/parsing/parser/type_parser.rs
+++ crates/sql/src/parsing/parser/type_parser.rs
@@ -66,6 +66,14 @@ pub trait TypeParser: TokenHelper {
             // Special types
             Token::Keyword(Keyword::Uuid) => DataType::Uuid,
             Token::Keyword(Keyword::Identity) => DataType::Identity,
+            Token::Keyword(Keyword::Private) => {
+                self.expect(Token::Keyword(Keyword::Key))?;
+                DataType::PrivateKey
+            }
+            Token::Keyword(Keyword::Public) => {
+                self.expect(Token::Keyword(Keyword::Key))?;
+                DataType::PublicKey
+            }
             Token::Keyword(Keyword::Date) => DataType::Date,
             Token::Keyword(Keyword::Time) => DataType::Time,
             Token::Keyword(Keyword::Timestamp) => DataType::Timestamp,
